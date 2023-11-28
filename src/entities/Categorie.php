03d21f8032b693d23fff27eb9de6db103a654e28
Regis <vavasseu2u@etu.univ-lorraine.fr>
2023-11-28 16:17:25 +0100

    début de la dénition de la table categorie avec doctrine

---
+++ src/entities/Categorie.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace catadoct\catalog\entities;
+
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\Table;
+
+#[Entity]
+#[Table(name: "categorie")]
+class Categorie
+{
+
+}
