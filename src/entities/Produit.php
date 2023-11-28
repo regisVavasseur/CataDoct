da3d7c33411e7ff20b481649715a389a7138bca3
Regis <vavasseu2u@etu.univ-lorraine.fr>
2023-11-28 16:09:49 +0100

    début de la dénition de la table produit avec doctrine

--- src/entities/Produit.php
+++ src/entities/Produit.php
@@ -2,8 +2,11 @@
 
 namespace catadoct\catalog\entities;
 
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\Table;
+
 #[Entity]
-#Table(name: "produit")
+#[Table(name: "produit")]
 class Produit
 {
 
