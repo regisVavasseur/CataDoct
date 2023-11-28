e296ab0a53252448031e4504637c890696140c58
Yvan <yvanquilliard@hotmail.fr>
2023-11-28 16:12:33 +0100

    Exo1

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
 require_once __DIR__ . DIRECTORY_SEPARATOR .'vendor/autoload.php';
+
+use catadoct\catalog\entities\Produit;
 use Doctrine\DBAL\DriverManager;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\ORMSetup;
@@ -13,3 +15,10 @@ $dbParams = parse_ini_file(__DIR__ . './db.env');
 $config = ORMSetup::createAttributeMetadataConfiguration($entity_path, $isDevMode);
 $connection = DriverManager::getConnection($dbParams, $config);
 $entityManager = new EntityManager($connection, $config);
+
+$product = new Produit();
+$entityManager->persist($product);
+
+print_r($entityManager->getRepository(Produit::class)->findAll());
+
+
